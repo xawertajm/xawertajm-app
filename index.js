d12a1f61e9fbc5177c90df17ead341430f0e4fdd
Matthew Croker <matthew.croker@ixaris.com>
2016-06-17 17:45:10 +0200

    now trying with send

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile('index.html', {root: __dirname});
+  response.send();
 });
 
 app.listen(app.get('port'), function() {
