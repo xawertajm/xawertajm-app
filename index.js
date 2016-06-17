1fc1c3489cb954db6c7decc824a5ad864e406e5c
Matthew Croker <matthew.croker@ixaris.com>
2016-06-17 18:00:07 +0200

    again with index.html

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send();
+  response.sendFile('index.html');
 });
 
 app.listen(app.get('port'), function() {
