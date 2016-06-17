c12e623d182d97deb39f4dc84dd1367fed132aef
Matthew Croker <matthew.croker@ixaris.com>
2016-06-17 18:17:08 +0200

    rotue

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
-app.get('/', function(request, response) {
+//app.get('/', );
+
+app.route('/*').get(function(request, response) {
   response.sendFile('index.html', {root: __dirname});
 });
 
