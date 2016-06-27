3aab3af16043037fb884aba3d6aba51af0501087
Matthew Croker <matthew.croker@ixaris.com>
2016-06-27 14:37:34 +0200

    Changed background to image to make app lighter to load

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var Index = require('./index/Index.react');
+var Background = require('./background/Background.react');
 var I18n = require('ixaris-uxf/lib/i18n');
 
 
@@ -23,4 +24,5 @@ require('ixaris-uxf/lib/style').Branding.load(require('./style/styleConfig.json'
 var div = document.createElement("div");
 document.body.appendChild(div);
 ReactDom.render(React.createElement(Index), div);
+ReactDom.render(React.createElement(Background), document.getElementById('bground'));
 I18n.setDefault("mt");
---
+++ app/background/Background.react.js
@@ -0,0 +1,48 @@
+var React = require('react');
+var PureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+var I18nMixin = require('ixaris-uxf').React.I18nMixin;
+var WeatherStore = require('../store/WeatherStore');
+
+function getCurrentWeather() {
+	return {
+		weather : WeatherStore.Store.getWeather()
+	};
+}
+
+var Background = React.createClass({
+
+	mixins: [PureRenderMixin, I18nMixin],
+
+	getInitialState: function () {
+		return {
+			weather : {
+				washCar : false,
+				predictionBasis : {
+					daysUntilRain : -1,
+				}
+			}
+		}
+	},
+	componentWillMount : function() {
+		WeatherStore.Store.addChangeListener(this._gotWeather);
+	},
+
+	componentDidMount : function() {
+		WeatherStore.Actions.getWeather();
+	},
+
+	componentWillUnmount : function () {
+		WeatherStore.Store.removeChangeListener(this._gotWeather);
+	},
+
+
+	render: require('./Background.rt.html'),
+	_gotWeather: function() {
+		var weatherState = getCurrentWeather();
+		this.setState(weatherState);
+	}
+
+});
+
+
+module.exports = Background;
